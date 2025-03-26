876028f4caba82306cca706d4b0da4d858e47e45
Samarth-Ad <samarthad950@gmail.com>
2025-03-26 23:38:59 +0530

    26-03-2025 11:40-PM

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/utils/theme/theme.dart';
+
+import 'app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+class TImages{
+  // --App logos
+  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png" ;
+  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png" ;
+
+  // --Social Logos
+  static const String google_logo = "assets/logos/google-icon.png" ;
+  static const String facebook_logo = "assets/logos/facebook-icon.png" ;
+
+}
